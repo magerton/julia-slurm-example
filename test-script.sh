1ff9b26e6cf4704927f5deb7e47b520b0d638345
Mark <mjagerton@ucdavis.edu>
2019-06-01 21:12:34 -0700

    remove email, add a bit more info from julia

--- test-script.sh
+++ test-script.sh
@@ -6,20 +6,22 @@
 # Job info
 #-----------------------
 
-#SBATCH --job-name=out
-#SBATCH --output out-%j.output
-#SBATCH --error out-%j.output
-#SBATCH --mail-user=mjagerton@ucdavis.edu
+#SBATCH --mail-user=EMAIL@DOMAIN.COM   # update email address to get notifications
 #SBATCH --mail-type=ALL
+#SBATCH --job-name=MY_GREAT_TEST_JOB
+
+# by default, slurm makes "slurm-%j.out" files w/ output. uncomment to change
+##SBATCH --output out-%j.out
+##SBATCH --error out-%j.out
 
 #-----------------------
 # Resource allocation
 #-----------------------
 
-#SBATCH --time=1:00:00     # in hours
-#SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --partition=high
+#SBATCH --time=1:00:00     # in d-hh:mm:ss
+#SBATCH --nodes=1          # single node
+#SBATCH --ntasks=4         # number of virtual cores
+#SBATCH --partition=med2   # partition
 
 ##SBATCH --mem=64000       # uncomment to max out RAM
 
@@ -28,14 +30,16 @@
 #-----------------------
 
 hostname
-module load julia/0.7.0
+module load julia/1.1.1
+
+# make job directory and switch to it
+mkdir ${SLURM_JOB_ID}
+cd ${SLURM_JOB_ID}
+
+echo "Starting job!!! ${SLURM_JOB_ID}"
+
+# print out environment variables related to SLURM_NTASKS
+julia -e 'println("\n"); [println((k,ENV[k],)) for k in keys(ENV) if occursin("SLURM_NTASKS",k)]; println("\n");'
 
 # run the script
-julia test-script.jl
-
-## this also works AS LONG AS we don't set the number of threads using
-##   ENV["SLURM_NTASKS"]
-#srun --ntasks=1 julia test-script.jl 
-
-## this DOES NOT. SLURM does everything multiple times
-## srun julia test-script.jl
+julia --optimize=3 test-script.jl
